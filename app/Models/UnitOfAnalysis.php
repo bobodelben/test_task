8fd732aa4e56e85d63210a3903eb6891b3b690f3
Roberto Del Ben <maia.softwareteam@gmail.com>
2023-10-27 14:09:41 +0200

    Models and migrations created

---
+++ app/Models/UnitOfAnalysis.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnitOfAnalysis extends Model
+{
+    use HasFactory;
+}
